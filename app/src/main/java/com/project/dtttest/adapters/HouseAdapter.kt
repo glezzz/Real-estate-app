84f584c73c4d406698b40032ebf3424a2b34c9b3
glezzz <alexglezk@gmail.com>
2021-06-19 20:56:31 +0200

    working on recyclerview

---
+++ app/src/main/java/com/project/dtttest/adapters/HouseAdapter.kt
@@ -0,0 +1,15 @@
+package com.project.dtttest.adapters
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.AsyncListDiffer
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.project.dtttest.R
+import com.project.dtttest.databinding.ItemHouseBinding
+import com.project.dtttest.model.HouseResponse
+
+class HouseAdapter() : RecyclerView.Adapter<HouseAdapter.HouseViewHolder>() {
+
+}
